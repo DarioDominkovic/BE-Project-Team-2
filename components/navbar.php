c7602b07b5319491a2480c7d37b8fa84cd4689c1
DarioDominkovic <dario.dominkovic@hotmail.com>
2023-08-17 13:00:31 +0200

    added nav.css

--- components/navbar.php
+++ components/navbar.php
@@ -9,19 +9,26 @@ $navbar = "
         <div class='collapse navbar-collapse justify-content-end' id='navbarSupportedContent'>
             <ul class='navbar-nav me-auto mb-2 mb-lg-0'>";
 
+$navbar .= "
+            <img class='logo' src='../pictures/logo.png' alt='Logo'>
+            </ul>";
+
 if (isset($_SESSION["user"]) || isset($_SESSION["adm"])) {
     $navbar .= "    
-                <li class='nav-item'>
-                    <a class='nav-link active' aria-current='page' href='{$raus}index.php'>Activities</a>
-                </li>";
+            <ul class='navbar-nav'>
+                <li class='nav-item'><a class='nav-link' href='{$raus}index.php'>Activities</a></li>
+            </ul>";
 }
+
+$navbar .= "
+            <ul class='navbar-nav'>";
 
 if (isset($_SESSION["user"])) {
     $id = $_SESSION["user"];
     $navbar .= "
                 <li class='nav-item'>
                     <a class='nav-link' href='{$raus}/crud_user/show.php?id=$id'>Profile</a>
-                </li>  
+                </li>
                 <li class='nav-item'>
                     <a class='nav-link' href='{$raus}routine.php'>My Routine</a>
                 </li>";
@@ -33,19 +40,29 @@ $navbar .= "
 if (isset($_SESSION["adm"])) {
     $id = $_SESSION["adm"];
     $navbar .= "
-            <li class='nav-item'>
-                <a class='nav-link' href='{$raus}dashboard.php'>Dashboard</a>
-            </li>";
-}
-
-if (isset($_SESSION["user"]) || isset($_SESSION["adm"])) {
-    $navbar .= "
-            <li class='nav-item'>
-                <a class='nav-link' href='{$raus}/login/logout.php?logout'>Logout</a>
-            </li>";
+            <ul class='navbar-nav'>
+                <li class='nav-item'>
+                    <a class='nav-link' href='{$raus}dashboard.php'>Dashboard</a>
+                </li>
+            </ul>";
 }
 
 $navbar .= "
+            <ul class='navbar-nav'>";
+
+if (isset($_SESSION["user"]) || isset($_SESSION["adm"])) {
+    $navbar .= "
+                <li class='nav-item'>
+                    <a class='logout' href='{$raus}/login/logout.php?logout'>Logout</a>
+                </li>";
+}
+
+$navbar .= "
+            </ul>
         </div>
     </div>
 </nav>";
+?>
+ <head>
+  <link rel="stylesheet" href="navbar.css">
+</head> 
