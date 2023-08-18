3ca870f042fb1b30f56c697072719d9a7ba40654
NawrasAlhosh <133000238+NawrasAlhosh@users.noreply.github.com>
2023-08-18 09:41:35 +0200

    resetPass layout

--- resetpassword.php
+++ resetpassword.php
@@ -40,20 +40,43 @@ if (isset($_POST["reset"])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>Password Reset</title>
 </head>
 
 <body>
 
-  <form method="POST">
-    <input type="text" name="oldPass" placeholder="your old password">
-    <p><?= $error ?></p>
-    <input type="text" name="newPass" placeholder="your new password"><br>
-    <input type="text" name="confirmPass" placeholder="confirm password">
-    <p><?= $errorNew ?></p>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            Password Reset
+          </div>
+          <div class="card-body">
+            <form method="POST">
+              <div class="mb-3">
+                <input type="text" class="form-control" name="oldPass" placeholder="Your old password">
+              </div>
+              <p><?= $error ?></p>
+              <div class="mb-3">
+                <input type="text" class="form-control" name="newPass" placeholder="Your new password">
+              </div>
+              <div class="mb-3">
+                <input type="text" class="form-control" name="confirmPass" placeholder="Confirm password">
+              </div>
+              <p><?= $errorNew ?></p>
+              <div class="d-grid gap-2">
+                <button class="btn btn-outline-primary" type="submit" name="reset">Reset password</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
-    <input type="submit" name="reset" value="Reset password">
-  </form>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
