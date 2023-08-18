85310a160e8286f2135dbfc15249c95d43de66c4
Mara Federmair <mara@klingt.org>
2023-08-18 21:39:35 +0200

    htmlroutine

--- routine.php
+++ routine.php
@@ -12,6 +12,14 @@ require_once "components/navbar.php";
 <!DOCTYPE html>
 <html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="routine.css">
+    <title>Morning Routine</title>
+</head>
+
 <body>
     <?php echo $navbar ?>
 
@@ -19,6 +27,8 @@ require_once "components/navbar.php";
         <div class="row">
             <?php
             $user_id = $_SESSION['user'];
+
+            // a as activity & ra as routine_activity
 
             $routine_activities_query = "SELECT a.* FROM `activity` a
                 JOIN `routine_activity` ra ON a.id = ra.fk_activity
