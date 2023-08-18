e65e06487ffe3dc77a957b160a6a8b5c1a7ca5b1
NawrasAlhosh <133000238+NawrasAlhosh@users.noreply.github.com>
2023-08-18 11:13:08 +0200

    enhancing update

--- crud_user/update.php
+++ crud_user/update.php
@@ -79,52 +79,53 @@ if ($result) {
 <body>
   <!-- navbar starts -->
   <?= $navbar ?>
-
   <!-- navbar ends -->
-
 
   <div class="container mt-5">
     <h2>Update Users</h2>
     <form method="post" enctype="multipart/form-data">
       <!-- Displaying the current values of the media record in the form for updating -->
       <div class="mb-3 mt-3">
-        <label for="fname" class="form-label">First name</label>
-        <input type="text" class="form-control" name="fname" area-describility="fname" id="fname" value="<?php echo $row["fname"]; ?>" />
+        <label for="fname" class="form-label">First Name</label>
+        <input type="text" class="form-control" name="fname" aria-describedby="fname" id="fname" value="<?php echo $row["fname"]; ?>" />
       </div>
       <div class="mb-3 mt-3">
-        <label for="lname" class="form-label">Last name</label>
+        <label for="lname" class="form-label">Last Name</label>
         <input type="text" class="form-control" id="lname" name="lname" value="<?php echo $row["lname"]; ?>" />
       </div>
 
       <div class="mb-3 mt-3">
-        <label for="username" class="form-label">User name</label>
-        <input type="text" class="form-control" name="username" area-describility="username" id="username" value="<?php echo $row["username"]; ?>" />
+        <label for="username" class="form-label">Username</label>
+        <input type="text" class="form-control" name="username" aria-describedby="username" id="username" value="<?php echo $row["username"]; ?>" />
       </div>
       <div class="mb-3 mt-3">
         <label for="email" class="form-label">Email</label>
-        <input type="text" class="form-control" name="email" area-describility="email" id="email" value="<?php echo $row["email"]; ?>" />
+        <input type="email" class="form-control" name="email" aria-describedby="email" id="email" value="<?php echo $row["email"]; ?>" />
       </div>
 
       <div class="mb-3 mt-3">
-        <label for="user_picture" class="form-label">User picture</label>
-        <input type="file" class="form-control" name="user_picture" area-describility="user_picture" id="user_picture" value="<?php echo $row["user_picture"]; ?>" />
+        <label for="user_picture" class="form-label">User Picture</label>
+        <input type="file" class="form-control" name="user_picture" aria-describedby="user_picture" id="user_picture" />
+        <small class="text-muted">Leave blank to keep the existing picture.</small>
       </div>
 
       <div class="mb-3 mt-3">
         <label for="create_date" class="form-label">Date of Creation</label>
-        <input type="text" class="form-control" name="create_date" area-describility="create_date" id="create_date" value="<?php echo $row["create_date"]; ?>" />
+        <input type="text" class="form-control" name="create_date" aria-describedby="create_date" id="create_date" value="<?php echo $row["create_date"]; ?>" disabled />
       </div>
       <div class="mb-3 mt-3">
         <label for="status" class="form-label">Status</label>
-        <input type="text" class="form-control" name="status" area-describility="status" id="status" value="<?php echo $row["status"]; ?>" />
+        <select class="form-select" name="status" aria-describedby="status" id="status">
+          <option value="active" <?php echo ($row["status"] === "active") ? "selected" : ""; ?>>Active</option>
+          <option value="inactive" <?php echo ($row["status"] === "inactive") ? "selected" : ""; ?>>Inactive</option>
+        </select>
       </div>
       <button type="submit" name="update_user" class="btn btn-outline-secondary">UPDATE USER</button>
     </form>
   </div>
 
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-pzjw5f+ua6qOJ6TnFdARDVx4PvQA4f6V9kD4Z5d4DFOxJ+5n6USvUew+OrCXaRkf" crossorigin="anonymous"></script>
 </body>
 
 </html>
