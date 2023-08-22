e0e3957f840e18ab189bc76eb0e07b148406452b
NawrasAlhosh <133000238+NawrasAlhosh@users.noreply.github.com>
2023-08-22 11:39:23 +0200

    mmm

--- login/forgotpassword.php
+++ login/forgotpassword.php
@@ -1,5 +1,5 @@
 <?php
-require_once "db_connect.php";
+require_once "../components/db_connect.php";
 if (isset($_POST["submit"])) {
   $email = $_POST["email"];
   $length = 10;
@@ -24,14 +24,32 @@ if (isset($_POST["submit"])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Password Reset</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-  <form method="post">
-    <input type="text" name="email" placeholder="Please type your email or username">
-    <input type="submit" value="Send new Password" name="submit">
-  </form>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Password Reset</h4>
+            <form method="post">
+              <div class="form-group">
+                <label for="email">Email or Username</label>
+                <input type="text" class="form-control" name="email" id="email" placeholder="Enter your email or username" required>
+              </div>
+              <br>
+              <button type="submit" class="btn btn-primary" name="submit">Send New Password</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
