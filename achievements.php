82d44b364d806e069b93a2d03a66c0334b842c4a
DarioDominkovic <dario.dominkovic@hotmail.com>
2023-08-24 11:50:33 +0200

    achivemnt format updated

--- achievements.php
+++ achievements.php
@@ -41,31 +41,44 @@ $num = 0;
             $sql = "SELECT aud.*, a.name AS activity_name FROM activity_user_done aud
                     JOIN activity a ON aud.fk_activity = a.id";
             $result = $connect->query($sql);
-
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
-                    $num = $row['Done'] * 10;
-                
+                    $num = $row['Done'];
+                    $format = 10;
+                    if ($num <= 10) {
+                         
+                        $format =10;
+                    };
+                    
+                    if ($row['Done'] > 10) {
+                        $format = 100; 
+
+                    }
+                    if ($row['Done'] > 100) {
+                        $format = 1000; 
+
+                    }
+                    $num = $num * 100 / $format;
                     echo "<div class='container' style='padding-bottom: 25px;'> ";
                     echo "<div class='progress-container' style='background-color:#455d7a; color:#e3e3e3; padding: 20px; border: 5px solid #233142 ;border-radius: 20px;'>";
-                
+            
                     echo "<div class='row'>";
                     echo "<div class='col-md-6'>";
                     echo "<h3>" . $row["activity_name"] . "</h3>";
                     echo "</div>";
                     echo "<div class='col-md-6'>";
-                    echo "<p class='text-end'>10 Days</p>";
+                    echo "<p class='text-end'> $format times</p>";
                     echo "</div>";
                     echo "</div>";
-                
+            
                     echo "<div class='progress'>";
                     echo "<div class='progress-bar' role='progressbar' style='width:" . $num . "%; background-color: #f95959;' aria-valuenow='" . $num . "' aria-valuemin='0' aria-valuemax='100'>" . $num . "%</div>";
                     echo "</div>";
-                
+            
                     echo "</div></div>";
                 }
-                
-            } else {
+            }
+             else {
                 echo "<tr><td colspan='2'>No activities found.</td></tr>";
             }
             ?>
